8863dc3357649fe3037aa173439d04e3772f42a5
eralpkor <eralp.ny@gmail.com>
2019-08-06 18:04:25 -0400

    start working on stretch-assignment

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,10 +1,36 @@
 
-
-
-function ContentDown(d) {
-  var obj = document.getElementById(d);
-  var currentPosition = parseInt(obj.style.top)
-  var amountToMove = 30
-
-  obj.style.top = currentPosition + amountToMove + "pt";
+const vars = {
+  x: 200, // Translates 200px from left to right
+  y: -200, // Translates 200px from bottom to top
+  rotation: 90 // Rotates 90 degrees
 }
+
+TweenMax.to(".block--red", 1, {
+  // x: 200, // Translates 200px from left to right
+  y: 200, // Translates 200px from bottom to top
+  rotation: 90 // Rotates 90 degrees
+  // rotation: 180,
+  // yoyo: true,
+  // repeat: -1,
+  // ease: Elastic.easeOut.config( 1, 0.1)
+}); 
+
+TweenMax.to(".block--blue", 1, {
+  // x: 200, // Translates 200px from left to right
+  y: -200, // Translates 200px from bottom to top
+  rotation: 90 // Rotates 90 degrees
+  // rotation: 180,
+  // yoyo: true,
+  // repeat: -1,
+  // ease: Elastic.easeOut.config( 1, 0.1)
+}); 
+
+TweenMax.to(".block--green", 1, {
+  // x: 200, // Translates 200px from left to right
+  y: -200, // Translates 200px from bottom to top
+  rotation: 90 // Rotates 90 degrees
+  // rotation: 180,
+  // yoyo: true,
+  // repeat: -1,
+  // ease: Elastic.easeOut.config( 1, 0.1)
+}); 
